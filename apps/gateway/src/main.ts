696270872c5350d4d49f2a37a0ecc0a3181e7579
pakseninkoboi <fajarr.space@gmail.com>
2024-08-10 17:48:24 +0700

    add: cd pipeline

--- apps/gateway/src/main.ts
+++ apps/gateway/src/main.ts
@@ -5,15 +5,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Configure CORS options
-  // const corsOptions: CorsOptions = {
-  //   origin: '*',
-  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  //   credentials: true,
-  //   allowedHeaders: 'Content-Type, Authorization, AccessToken, RefreshToken', // Add AccessToken to allowed headers
-  // };
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization, AccessToken, RefreshToken', // Add AccessToken to allowed headers
+  };
 
   // Enable CORS with the specified options
-  // app.enableCors(corsOptions);
+  app.enableCors(corsOptions);
   await app.listen(4003, '0.0.0.0'); // Update port to 3000
 }
 bootstrap();
